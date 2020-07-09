d176004ec7136f202a69b654b13a669c155ded4d
Thiago <thiagommilitao@gmail.com>
2020-07-09 12:07:54 -0300

    Created grid and made it pixelate on hovering

--- script/script.js
+++ script/script.js
@@ -1,11 +1,41 @@
+const body = document.querySelector("body");
+const restart = document.createElement("button");
+restart.setAttribute("onClick", "window.location.reload();")
+restart.textContent = "Restart";
+body.prepend(restart);
 const container = document.querySelector(".container");
+container.style.cssText = "background-color: #888888; width: 800px; height: 800px; border-right: 1px solid black; border-bottom: 1px solid black;";
+
+
+
+let amount = prompt("How many rows would you like?", "16");
 
 function createGrid(amount) {
     for (let i = 0; i < amount * amount; i++) {
         const divs = document.createElement("div");
         container.appendChild(divs);
-        divs.setAttribute("id", "grid");
-        divs.textContent="."
+        divs.setAttribute("class", "grid");
+        divs.textContent = ".";
+        //divs.textContent = `${Math.ceil((i+1)/16)}` + "x" + `${i+1}`;
+
     }
 }
-createGrid(16);
+createGrid(amount);
+
+function style(){
+    const grid = document.getElementsByClassName("grid");
+    for (let k=0; k<grid.length; k++){
+        grid[k].style.cssText = `display: inline-block; width: ${800/amount}px; height: ${800/amount}px; background-color: #888888; border-left: 0.1px solid black; border-top: 0.1px solid black; margin: none; box-sizing: border-box; color: #888888;`;
+    }
+}
+style();
+
+function pixelate() {
+    const test = document.getElementsByClassName("grid");
+    for (let j = 0; j < test.length; j++) {
+        test[j].addEventListener("mouseover", function (e) {
+            e.target.style.cssText = `display: inline-block; width: ${800/amount}px; height: ${800/amount}px; background-color: #000000; border-left: 0.1px solid black; border-top: 0.1px solid black; margin: none; box-sizing: border-box; color: #000000;`;
+        })
+    }
+}
+pixelate();
